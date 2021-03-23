081eabc4c8865f6b90fa9a55207eb641732d0114
LeeAnn <lmkabler@madisoncollege.edu>
2021-03-23 16:18:20 -0500

    created context.xml and added information to role and user tables

--- src/main/java/controller/SearchBooks.java
+++ src/main/java/controller/SearchBooks.java
@@ -1,2 +1,34 @@
-package controller;public class SearchBooks {
+package controller;
+
+import entity.Info;
+import persistence.GenericDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+
+@WebServlet(
+        urlPatterns = {"/searchBooks"}
+)
+
+public class SearchBooks extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        GenericDao dao = new GenericDao(Info.class);
+
+        if (req.getParameter ("submit").equals("search")) {
+            req.setAttribute("users", dao.getByPropertyLike("title", req.getParameter("searchTerm")));
+        } else {
+            req.setAttribute("users", dao.getAll());
+        }
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/results.jsp");
+        dispatcher.forward(req, resp);
+    }
 }
