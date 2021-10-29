1fa2e5dfbbd7d0a69b9e7c750ecc21ea3c745787
LeeAnn <lmkabler@madisoncollege.edu>
2021-10-28 20:44:15 -0500

    updated mysqldump with new book suggestions data. Created bookSuggestions.java class

---
+++ src/main/java/com/leeannjakel/entity/BookSuggestions.java
@@ -0,0 +1,53 @@
+package com.leeannjakel.entity;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * The type Author.
+ *
+ * @author LeeAnn Jakel
+ */
+@Entity(name="BookSuggestions")
+@Table(name="bookSuggestions")
+
+/**
+ * Lombok
+ */
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+public class BookSuggestions {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    @Column(name = "title")
+    @NonNull
+    private String title;
+
+    @Column(name = "isbn")
+    @NonNull
+    private String isbn;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    @NonNull
+    private Author author;
+
+    @ManyToOne
+    @JoinColumn(name = "genre_id")
+    @NonNull
+    private Genre genre;
+
+    @Column(name = "description")
+    @NonNull
+    private String description;
+
+}
